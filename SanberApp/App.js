2a3137f60c3e3ea813afa6d9254df609ce28078c
Albert Johannes <albert@espay.id>
2021-01-26 23:37:26 +0700

    submit tugas 12

--- SanberApp/App.js
+++ SanberApp/App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity } from 'react-native';
+import { Ionicons, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import App from './Tugas/Tugas12/App.js';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello brotha! Yo!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ SanberApp/Tugas/Tugas12/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, SafeAreaView, Image, TouchableOpacity, FlatList } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import VideoItem from '../Tugas12/components/videoItem';
+import data from './data.json';
+
+export default class App extends Component {
+    render() {
+        // alert(data.kind)
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.navBar}>
+                    <Image source={require('../../Tugas/Tugas12/images/logo.png')} style={{ width: 98, height: 22 }} />
+                    <View style={styles.rightNav}>
+                        <TouchableOpacity>
+                            <MaterialIcons style={styles.navItem} name="search" size={25} />
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <MaterialIcons style={styles.navItem} name="account-circle" size={25} />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.body}>
+                    {/* <VideoItem video={data.items[0]} /> */}
+                    <FlatList
+                        data={data.items}
+                        renderItem={(video) => <VideoItem video={video.item} />}
+                        keyExtractor={(item) => item.id}
+                        ItemSeparatorComponent={() => <View style={{ height: 0.5, backgroundColor: '#e5e5e5' }} />}
+                    />
+                </View>
+                <View style={styles.tabBar}>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <MaterialIcons name="home" size={25} />
+                        <Text style={styles.tabTitle}>Home</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <MaterialIcons name="whatshot" size={25} />
+                        <Text style={styles.tabTitle}>Trending</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <MaterialIcons name="subscriptions" size={25} />
+                        <Text style={styles.tabTitle}>Subscriptions</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.tabItem}>
+                        <MaterialIcons name="folder" size={25} />
+                        <Text style={styles.tabTitle}>Library</Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    navBar: {
+        height: 55,
+        backgroundColor: 'white',
+        //ios only start
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        //ios only end
+        elevation: 10,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    rightNav: {
+        flexDirection: 'row'
+    },
+    navItem: {
+        marginLeft: 25
+    },
+    body: {
+        flex: 1
+    },
+    tabBar: {
+        backgroundColor: 'white',
+        height: 60,
+        borderTopWidth: 0.5,
+        borderColor: '#E5E5E5',
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    tabItem: {
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    tabTitle: {
+        fontSize: 11,
+        color: '#3c3c3c',
+        paddingTop: 4
+    }
+})
